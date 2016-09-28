f2a0b5111eeb8b9d5c6150c289066a8b6d9b26d6
Logvinov Leon <logvinov.leon@gmail.com>
2016-09-27 20:10:31 -0700

    Keep API as simple as possible

--- definitions/definitions.go
+++ definitions/definitions.go
@@ -1,9 +1,7 @@
 package definitions
 
 type EmailSendPayload struct {
-	To      []string
-	Cc      []string
-	Bcc     []string
+	To      string
 	Subject string
 	Body    string
 	PreferredProvider string
